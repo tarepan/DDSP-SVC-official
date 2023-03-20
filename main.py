930b369fe589b3a7543379561080b9ae56bdaf78
tarepan <tarepan5884@gmail.com>
2023-03-20 13:01:07 +0000

    Refactor inference

--- main.py
+++ main.py
@@ -13,113 +13,30 @@ from ddsp.core import upsample
 from enhancer import Enhancer
 from tqdm import tqdm
 
+
 def parse_args(args=None, namespace=None):
     """Parse command-line arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-m",
-        "--model_path",
-        type=str,
-        required=True,
-        help="path to the model file",
-    )
-    parser.add_argument(
-        "-i",
-        "--input",
-        type=str,
-        required=True,
-        help="path to the input audio file",
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        type=str,
-        required=True,
-        help="path to the output audio file",
-    )
-    parser.add_argument(
-        "-id",
-        "--spk_id",
-        type=str,
-        required=False,
-        default=1,
-        help="speaker id (for multi-speaker model) | default: 1",
-    )
-    parser.add_argument(
-        "-mix",
-        "--spk_mix_dict",
-        type=str,
-        required=False,
-        default="None",
-        help="mix-speaker dictionary (for multi-speaker model) | default: None",
-    )
-    parser.add_argument(
-        "-k",
-        "--key",
-        type=str,
-        required=False,
-        default=0,
-        help="key changed (number of semitones) | default: 0",
-    )
-    parser.add_argument(
-        "-e",
-        "--enhance",
-        type=str,
-        required=False,
-        default='false',
-        help="true or false | default: false",
-    )
-    parser.add_argument(
-        "-pe",
-        "--pitch_extractor",
-        type=str,
-        required=False,
-        default='crepe',
-        help="pitch extrator type: parselmouth, dio, harvest, crepe (default)",
-    )
-    parser.add_argument(
-        "-fmin",
-        "--f0_min",
-        type=str,
-        required=False,
-        default=50,
-        help="min f0 (Hz) | default: 50",
-    )
-    parser.add_argument(
-        "-fmax",
-        "--f0_max",
-        type=str,
-        required=False,
-        default=1100,
-        help="max f0 (Hz) | default: 1100",
-    )
-    parser.add_argument(
-        "-th",
-        "--threhold",
-        type=str,
-        required=False,
-        default=-60,
-        help="response threhold (dB) | default: -60",
-    )
-    parser.add_argument(
-        "-eak",
-        "--enhancer_adaptive_key",
-        type=str,
-        required=False,
-        default=0,
-        help="adapt the enhancer to a higher vocal range (number of semitones) | default: 0",
-    )
+    parser.add_argument("-m",    "--model_path",      type=str, required=True,                   help="path to the model file")
+    parser.add_argument("-i",    "--input",           type=str, required=True,                   help="path to the input audio file")
+    parser.add_argument("-o",    "--output",          type=str, required=True,                   help="path to the output audio file")
+    parser.add_argument("-id",   "--spk_id",          type=str, required=False, default=1,       help="speaker id (for multi-speaker model) | default: 1")
+    parser.add_argument("-mix",  "--spk_mix_dict",    type=str, required=False, default="None",  help="mix-speaker dictionary (for multi-speaker model) | default: None")
+    parser.add_argument("-k",    "--key",             type=str, required=False, default=0,       help="key changed (number of semitones) | default: 0")
+    parser.add_argument("-e",    "--enhance",         type=str, required=False, default='false', help="true or false | default: false")
+    parser.add_argument("-pe",   "--pitch_extractor", type=str, required=False, default='crepe', help="pitch extrator type: parselmouth, dio, harvest, crepe (default)")
+    parser.add_argument("-fmin", "--f0_min",          type=str, required=False, default=50,      help="min f0 (Hz) | default: 50")
+    parser.add_argument("-fmax", "--f0_max",          type=str, required=False, default=1100,    help="max f0 (Hz) | default: 1100")
+    parser.add_argument("-th",   "--threhold",        type=str, required=False, default=-60,     help="response threhold (dB) | default: -60")
+    parser.add_argument("-eak", "--enhancer_adaptive_key", type=str, required=False, default=0,  help="adapt the enhancer to a higher vocal range (number of semitones) | default: 0")
+    parser.add_argument("-sr",  "--sampling_rate",    type=int, required=False, default=44100,   help="Audio sampling rate")
     return parser.parse_args(args=args, namespace=namespace)
-
     
 def split(audio, sample_rate, hop_size, db_thresh = -40, min_len = 5000):
-    slicer = Slicer(
-                sr=sample_rate,
-                threshold=db_thresh,
-                min_length=min_len)       
+    slicer = Slicer(sr=sample_rate, threshold=db_thresh, min_length=min_len)       
     chunks = dict(slicer.slice(audio))
     result = []
-    for k, v in chunks.items():
+    for _, v in chunks.items():
         tag = v["split_time"].split(",")
         if tag[0] != tag[1]:
             start_frame = int(int(tag[0]) // hop_size)
@@ -151,29 +68,16 @@ if __name__ == '__main__':
     # load ddsp model
     model, args = load_model(cmd.model_path, device=device)
     
-    # load input
-    audio, sample_rate = librosa.load(cmd.input, sr=None)
-    if len(audio.shape) > 1:
-        audio = librosa.to_mono(audio)
-    hop_size = args.data.block_size * sample_rate / args.data.sampling_rate
+    # load input :: (T,)
+    audio, sr_i = librosa.load(cmd.input, sr=None, mono=True)
+    hop_size = args.data.block_size * sr_i / args.data.sampling_rate
     
-    # extract f0
+    # Analysis
+    ## fo
     print('Pitch extractor type: ' + cmd.pitch_extractor)
-    pitch_extractor = F0_Extractor(
-                        cmd.pitch_extractor, 
-                        sample_rate, 
-                        hop_size, 
-                        float(cmd.f0_min), 
-                        float(cmd.f0_max))
-    print('Extracting the pitch curve of the input audio...')
-    f0 = pitch_extractor.extract(audio, uv_interp = True, device = device)
-    f0 = torch.from_numpy(f0).float().to(device).unsqueeze(-1).unsqueeze(0)
-   
-    # key change
-    f0 = f0 * 2 ** (float(cmd.key) / 12)
-    
-    # extract volume 
-    print('Extracting the volume envelope of the input audio...')
+    pitch_extractor = F0_Extractor(cmd.pitch_extractor, sr_i, hop_size, float(cmd.f0_min), float(cmd.f0_max))
+    f0 = torch.from_numpy(pitch_extractor.extract(audio, uv_interp = True, device = device)).float().to(device).unsqueeze(-1).unsqueeze(0)
+    ## Volume 
     volume_extractor = Volume_Extractor(hop_size)
     volume = volume_extractor.extract(audio)
     mask = (volume > 10 ** (float(cmd.threhold) / 20)).astype('float')
@@ -182,64 +86,61 @@ if __name__ == '__main__':
     mask = torch.from_numpy(mask).float().to(device).unsqueeze(-1).unsqueeze(0)
     mask = upsample(mask, args.data.block_size).squeeze(-1)
     volume = torch.from_numpy(volume).float().to(device).unsqueeze(-1).unsqueeze(0)
-    
-    # load units encoder
-    units_encoder = Units_Encoder(
-                        args.data.encoder, 
-                        args.data.encoder_ckpt, 
-                        args.data.encoder_sample_rate, 
-                        args.data.encoder_hop_size, 
-                        device = device)
-                        
-    # load enhancer
-    if cmd.enhance == 'true':
-        print('Enhancer type: ' + args.enhancer.type)
-        enhancer = Enhancer(args.enhancer.type, args.enhancer.ckpt, device=device)
-    else:
-        print('Enhancer type: none (using raw output of ddsp)')
-    
-    # speaker id or mix-speaker dictionary
+
+    # Modification
+    ## fo - key change
+    f0 = f0 * 2 ** (float(cmd.key) / 12)
+    ## Speaker - speaker id or mix-speaker dictionary
     spk_mix_dict = literal_eval(cmd.spk_mix_dict)
     if spk_mix_dict is not None:
         print('Mix-speaker mode')
     else:
         print('Speaker ID: '+ str(int(cmd.spk_id)))        
     spk_id = torch.LongTensor(np.array([[int(cmd.spk_id)]])).to(device)
+
+    # load units encoder
+    units_encoder = Units_Encoder(args.data.encoder, args.data.encoder_ckpt, args.data.encoder_sample_rate, args.data.encoder_hop_size, device = device)
+
+    # load enhancer
+    if cmd.enhance == 'true':
+        print('Enhancer type: ' + args.enhancer.type)
+        enhancer = Enhancer(args.enhancer.type, args.enhancer.ckpt, device=device)
+    else:
+        print('Enhancer type: none (using raw output of ddsp)')
+
     # forward and save the output
     result = np.zeros(0)
     current_length = 0
-    segments = split(audio, sample_rate, hop_size)
+    segments = split(audio, sr_i, hop_size)
     print('Cut the input audio into ' + str(len(segments)) + ' slices')
     with torch.no_grad():
         for segment in tqdm(segments):
             start_frame = segment[0]
             seg_input = torch.from_numpy(segment[1]).float().unsqueeze(0).to(device)
-            seg_units = units_encoder.encode(seg_input, sample_rate, hop_size)
-           
-            seg_f0 = f0[:, start_frame : start_frame + seg_units.size(1), :]
+            # Analysis
+            ## Unit
+            seg_units = units_encoder.encode(seg_input, sr_i, hop_size)
+            ## fo/Volume
+            seg_f0     =     f0[:, start_frame : start_frame + seg_units.size(1), :]
             seg_volume = volume[:, start_frame : start_frame + seg_units.size(1), :]
             
-            seg_output, _, (s_h, s_n) = model(seg_units, seg_f0, seg_volume, spk_id = spk_id, spk_mix_dict = spk_mix_dict)
+            # Synthesis
+            ## DDSP
+            seg_output = model(seg_units, seg_f0, seg_volume, spk_id = spk_id, spk_mix_dict = spk_mix_dict)[0]
             seg_output *= mask[:, start_frame * args.data.block_size : (start_frame + seg_units.size(1)) * args.data.block_size]
-            
+            ## Enhancer
             if cmd.enhance == 'true':
-                seg_output, output_sample_rate = enhancer.enhance(
-                                                            seg_output, 
-                                                            args.data.sampling_rate, 
-                                                            seg_f0, 
-                                                            args.data.block_size, 
-                                                            adaptive_key = float(cmd.enhancer_adaptive_key))
+                seg_output, sr_o = enhancer.enhance(seg_output, args.data.sampling_rate, seg_f0, args.data.block_size, adaptive_key = float(cmd.enhancer_adaptive_key))
             else:
-                output_sample_rate = args.data.sampling_rate
-            
+                sr_o = args.data.sampling_rate
             seg_output = seg_output.squeeze().cpu().numpy()
             
-            silent_length = round(start_frame * args.data.block_size * output_sample_rate / args.data.sampling_rate) - current_length
+            silent_length = round(start_frame * args.data.block_size * sr_o / args.data.sampling_rate) - current_length
             if silent_length >= 0:
                 result = np.append(result, np.zeros(silent_length))
                 result = np.append(result, seg_output)
             else:
                 result = cross_fade(result, seg_output, current_length + silent_length)
             current_length = current_length + silent_length + len(seg_output)
-        sf.write(cmd.output, result, output_sample_rate)
+        sf.write(cmd.output, result, sr_o)
     
